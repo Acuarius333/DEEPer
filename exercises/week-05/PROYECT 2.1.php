c25bab7b36d8574f3dbbf493bc9fc2da032c6087
user <user@192.168.1.137>
2021-03-04 09:24:12 +0000

    Final Project - Layout created - 04-03-2021

--- exercises/week-05/PROYECT 2.1.php
+++ exercises/week-05/PROYECT 2.1.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <meta charset="UTF-8">
 <head>
-
-    <title>British Shorthair</title>
+    <title>Project</title>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
